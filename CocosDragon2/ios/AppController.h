b441e8c40d1e2207fdf91e6f137ebfaf6a1aef03
170warship <j10a.student@sina.com>
2013-07-26 21:30:30 +0800

    Initial Commit

---
+++ CocosDragon2/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  CocosDragon2AppController.h
+//  CocosDragon2
+//
+//  Created by 170warship on 7/26/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ CocosDragon2/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CocosDragon2AppController.h
+//  CocosDragon2
+//
+//  Created by 170warship on 7/26/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
